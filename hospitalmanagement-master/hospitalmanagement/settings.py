b341cf671681577557f4ee1d39b9a48f55996e4a
hussainanjil <hussainanjil@gmail.com>
2021-08-14 02:34:05 +0500

    Commit

--- hospitalmanagement-master/hospitalmanagement/settings.py
+++ hospitalmanagement-master/hospitalmanagement/settings.py
@@ -126,8 +126,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[STATIC_DIR,]
 
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
-#STATIC_ROOT = "/Users/DELL/Documents/GitHub/hussainanjil.github.io-/hospitalmanagement-master/static/"
+MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = "/Users/DELL/Documents/GitHub/hussainanjil.github.io-/hospitalmanagement-master/static/"
 
 
 
