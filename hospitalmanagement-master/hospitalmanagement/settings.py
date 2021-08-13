7e6e4ae511d1c544bb64fc5080608872ab268db2
hussainanjil <hussainanjil@gmail.com>
2021-08-13 23:59:48 +0500

    Commit

--- hospitalmanagement-master/hospitalmanagement/settings.py
+++ hospitalmanagement-master/hospitalmanagement/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[STATIC_DIR,]
+#STATICFILES_DIRS=[STATIC_DIR,]
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+#MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
 
 
