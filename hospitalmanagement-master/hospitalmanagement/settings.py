cb08d79c3f44c9bd46ff1cd126c5a049f5bf5e60
hussainanjil <hussainanjil@gmail.com>
2021-08-14 02:14:57 +0500

    Commit

--- hospitalmanagement-master/hospitalmanagement/settings.py
+++ hospitalmanagement-master/hospitalmanagement/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[STATIC_DIR,]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = "/health-assist01.herokuapp.com/static/"
 
 
 
