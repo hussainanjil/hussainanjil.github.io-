f5649d6e7c956e16460cfea6f237f3dc8f8b8359
hussainanjil <hussainanjil@gmail.com>
2021-08-15 03:53:33 +0500

    Commit

--- hospitalmanagement-master/hospitalmanagement/settings.py
+++ hospitalmanagement-master/hospitalmanagement/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['111.88.22.116','health-assist01.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','health-assist01.herokuapp.com']
 
 
 # Application definition
